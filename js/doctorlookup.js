74e6fbe7e4c6def95aa1485d3f84e5ab514927c4
Rakhee Gandhi <rpgandhi@gmail.com>
2017-12-17 15:13:09 -0800

    Cannot get API to return data. Have tried several methods. Not sure how to fix the problem.

--- js/doctorlookup.js
+++ js/doctorlookup.js
@@ -16,18 +16,48 @@ getDataSymptoms(userSymptom, displaySymptomData) {
 
   // https://api.betterdoctor.com/2016-03-01/doctors?query=johnson&location=or-portland&sort=best-match-desc&skip=0&limit=10&user_key=65f508702db150e33c63feadd03103e1
 
-getDataDoctors(userDoctor, displayDoctorData) {
-  console.log("getdatadoctorsFunctionworking");
-  $.get(`https://api.betterdoctor.com/2016-03-01/
-    doctors?query=${userDoctor}&location=or-portland&sort=best-match-desc&skip=0&limit=10&user_key=${apiKey}`)
-    .then(function(response) {
-      displayDoctorData(response);
-    })
-    .fail(function() {
+  getDataDoctors(userDoctor, displayDoctorData){
+    let promise = new Promise(function(resolve, reject){
+      let request = new XMLHttpRequest();
+      let url = `https://api.betterdoctor.com/2016-03-01/
+        doctors?query=${userDoctor}&location=or-portland&sort=best-match-desc&skip=0&limit=10&user_key=${apiKey}`;
+      request.onload = function(){
+        if (this.status === 200) {
+          resolve(request.response);
+        }
+        else {
+          reject(Error(request.statusText));
+        }
+      };
+        request.open("GET", url, true);
+        request.send();
+      });
 
-      console.log('something went wrong');
-    });
+      promise.then(function(response){
+        body = JSON.parse(response);
+      });
+    }
   }
-}
+
+// getDataDoctors(userDoctor, displayDoctorData) {
+//   console.log("getdatadoctorsFunctionworking");
+//   $.get(`https://api.betterdoctor.com/2016-03-01/
+//     doctors?query=${userDoctor}&location=or-portland&sort=best-match-desc&skip=0&limit=10&user_key=${apiKey}`)
+//     .then(function(response) {
+//       displayDoctorData(response);
+//     })
+//     .fail(function() {
+//
+//       console.log('something went wrong');
+//     });
+//   }
+
 
 // exports.applicationModule = ApplicationModule;
+
+//
+// ("https://api.betterdoctor.com/2016-03-01/doctors?query=johnson&location=or-portland&sort=best-match-desc&skip=0&limit=10&user_key=65f508702db150e33c63feadd03103e1")
+//
+// ("https://api.betterdoctor.com/2016-03-01/doctors?query=smith&location=or-portland&sort=best-match-desc&skip=0&limit=10&user_key=65f508702db150e33c63feadd03103e1")
+//
+// ("https://api.betterdoctor.com/2016-03-01/doctors?query=smith&location=or-portland&sort=best-match-desc&skip=0&limit=10&user_key=65f508702db150e33c63feadd03103e1")
